21d0aac09be137a1004a6747c39e4a49bdbdbeb6
Basix <basix@basix.tech>
2025-07-14 22:58:07 +0900

    Update banner text

--- kernel/main.c
+++ kernel/main.c
@@ -14,7 +14,7 @@ main()
     consoleinit();
     printfinit();
     printf("\n");
-    printf("xv6 kernel is booting\n");
+    printf("basix xv6 kernel is booting\n");
     printf("\n");
     kinit();         // physical page allocator
     kvminit();       // create kernel page table
@@ -41,5 +41,5 @@ main()
     plicinithart();   // ask PLIC for device interrupts
   }
 
-  scheduler();        
+  scheduler();
 }
