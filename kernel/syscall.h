6fc8b812037d39a1db9bb7c1964b3b53228606b8
Basix <basix@basix.tech>
2025-07-15 00:04:19 +0900

    Revert "Use FreeBSD-style syscalls"
    This reverts commit a77c9330954c3deeaa49bf766d457b618aa643a1.

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -1,25 +1,23 @@
 // System call numbers
-#define SYS_exit    1
-#define SYS_fork    2
-#define SYS_read    3
-#define SYS_write   4
-#define SYS_open    5
-#define SYS_close   6
-#define SYS_wait    7 // wait4 on freebsd
-#define SYS_unlink  9
-#define SYS_link    10
-#define SYS_exec    11 // obsolete on freebsd
-#define SYS_chdir   12
-#define SYS_mknod   14 // freebsd11_mknod
-#define SYS_getpid  20
-#define SYS_kill    37
-#define SYS_dup     41
-#define SYS_pipe    42 // freebsd10_pipe on freebsd
-#define SYS_reboot  55
-#define SYS_fstat   62 // old fstat on freebsd
-#define SYS_sbrk    69
-#define SYS_mkdir   136
-
-// non-standard calls
-#define SYS_sleep   613
-#define SYS_uptime  614
+#define SYS_fork    1
+#define SYS_exit    2
+#define SYS_wait    3
+#define SYS_pipe    4
+#define SYS_read    5
+#define SYS_kill    6
+#define SYS_exec    7
+#define SYS_fstat   8
+#define SYS_chdir   9
+#define SYS_dup    10
+#define SYS_getpid 11
+#define SYS_sbrk   12
+#define SYS_sleep  13
+#define SYS_uptime 14
+#define SYS_open   15
+#define SYS_write  16
+#define SYS_mknod  17
+#define SYS_unlink 18
+#define SYS_link   19
+#define SYS_mkdir  20
+#define SYS_close  21
+#define SYS_reboot 22
