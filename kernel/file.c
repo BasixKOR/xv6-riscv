3f5aaca80d5bf31e96b536427e54135ef92d6ed4
Basix <basix@basix.tech>
2025-07-18 21:22:41 +0900

    init spinlock

--- kernel/file.c
+++ kernel/file.c
@@ -22,7 +22,9 @@ struct {
 void
 fileinit(void)
 {
+  extern struct spinlock readcountlock;
   initlock(&ftable.lock, "ftable");
+  initlock(&readcountlock, "readcount");
 }
 
 // Allocate a file structure.
@@ -89,7 +91,7 @@ filestat(struct file *f, uint64 addr)
 {
   struct proc *p = myproc();
   struct stat st;
-  
+
   if(f->type == FD_INODE || f->type == FD_DEVICE){
     ilock(f->ip);
     stati(f->ip, &st);
@@ -179,4 +181,3 @@ filewrite(struct file *f, uint64 addr, int n)
 
   return ret;
 }
-
